12d22784d69e969939e814ea372c7bb8e71e3e7a
Jiwon Juliet Yoon <jiwonjulietyoon@gmail.com>
2019-06-16 20:52:03 +0900

    Add more towers to castle

--- js/castle.js
+++ js/castle.js
@@ -3,12 +3,12 @@
 
 
 $(document).ready(function(){
-    var towerMidWidth = $(".towerMid").width();
-    $(".towerTri").css({
-        "borderBottomWidth": towerMidWidth,
-        "borderLeftWidth": towerMidWidth / 2,
-        "borderRightWidth": towerMidWidth / 2,
-    });
+    // var towerMidWidth = $(".towerMid").width();
+    // $(".towerTri").css({
+    //     "borderBottomWidth": towerMidWidth,
+    //     "borderLeftWidth": towerMidWidth / 2,
+    //     "borderRightWidth": towerMidWidth / 2,
+    // });
     var castleSideWidth = $(".sideBd").width();
     $(".sideShield").css({
         "borderTopWidth": castleSideWidth * 0.2,
@@ -18,12 +18,12 @@ $(document).ready(function(){
     
     
     $(window).resize(function(){
-        var towerMidWidth = $(".towerMid").width()
-        $(".towerTri").css({
-            "borderBottomWidth": towerMidWidth,
-            "borderLeftWidth": towerMidWidth / 2,
-            "borderRightWidth": towerMidWidth / 2,
-        });
+        // var towerMidWidth = $(".towerMid").width()
+        // $(".towerTri").css({
+        //     "borderBottomWidth": towerMidWidth,
+        //     "borderLeftWidth": towerMidWidth / 2,
+        //     "borderRightWidth": towerMidWidth / 2,
+        // });
         var castleSideWidth = $(".sideBd").width();
         $(".sideShield").css({
             "borderTopWidth": castleSideWidth * 0.2,
@@ -33,18 +33,18 @@ $(document).ready(function(){
     });
 });
 
-window.addEventListener("load", function(){
-	var delay = 0;
-	var nodes = document.querySelectorAll(".animate");
-	for(var i=0; i<nodes.length; i++){
-		var words = nodes[i].innerText.split(" ");
-		nodes[i].innerHTML = "";
-        for(var i2=0; i2<words.length; i2++){
-                    var item = document.createElement("span");
-                    item.innerText = words[i2];
-                    var calc = (delay+((nodes.length + i2)/3));
-            item.style.animationDelay = calc+"s";
-                    nodes[i].appendChild(item);
-        }
-    }
-});
+// window.addEventListener("load", function(){
+// 	var delay = 0;
+// 	var nodes = document.querySelectorAll(".animate");
+// 	for(var i=0; i<nodes.length; i++){
+// 		var words = nodes[i].innerText.split(" ");
+// 		nodes[i].innerHTML = "";
+//         for(var i2=0; i2<words.length; i2++){
+//                     var item = document.createElement("span");
+//                     item.innerText = words[i2];
+//                     var calc = (delay+((nodes.length + i2)/3));
+//             item.style.animationDelay = calc+"s";
+//                     nodes[i].appendChild(item);
+//         }
+//     }
+// });
