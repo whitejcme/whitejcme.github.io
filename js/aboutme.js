81641bbf23843ac6e980052ded17ac10f53ac63a
Jiwon Juliet Yoon <jiwonjulietyoon@gmail.com>
2019-06-25 17:54:34 +0900

    Start working on skills page

--- js/aboutme.js
+++ js/aboutme.js
@@ -6,24 +6,28 @@ const aboutmeApp = new Vue({
         allText: [
             {
                 preview: "FE Web",
+                html: "<span style='color: red;'>TEST</span>",
                 word: "Front-end Web",
                 blurb: "HTML CSS JS; immense interest in UI/UX Design",
                 current: false
             },
             {
                 preview: "TCK",
+                html: "<span style='color: blue;'>TEST</span>",
                 word: "Third Culture Kid",
                 blurb: "Born in Korea, also raised in America, Germany, Malaysia",
                 current: false
             },
             {
                 preview: "Honest",
+                html: "<span style='color: purple;'>TEST</span>",
                 word: "Honest",
                 blurb: "What my friends say about me :)",
                 current: false
             },
             {
                 preview: "MBTI",
+                html: "<span style='color: green;'>TEST</span>",
                 word: "ESTJ: The Executive",
                 blurb: "Insight of the Day: Executives are the least likely personality type to feel overwhelmed by their to-do list.",
                 current: false
