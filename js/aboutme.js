9caf434791bab09ac8aeb5302d6b525ff1aff9dc
Jiwon Juliet Yoon <jiwonjulietyoon@gmail.com>
2019-06-26 17:50:35 +0900

    Apply icons to about me page

--- js/aboutme.js
+++ js/aboutme.js
@@ -6,28 +6,28 @@ const aboutmeApp = new Vue({
         allText: [
             {
                 preview: "FE Web",
-                html: "<span style='color: red;'>TEST</span>",
+                html: "<div class='wordBox crown'><div class='rightTriangle triL'><div class='crownTop'></div></div><div class='rightTriangle triR'><div class='crownTop'></div></div><div class='triCenter'><div class='crownTop'></div></div><div class='preview'>FE Web</div></div>",
                 word: "Front-end Web",
                 blurb: "HTML CSS JS; immense interest in UI/UX Design",
                 current: false
             },
             {
                 preview: "TCK",
-                html: "<span style='color: blue;'>TEST</span>",
+                html: "<div class='wordBox diamond'><div class='cut-diamond'></div><div class='preview'>TCK</div></div>",
                 word: "Third Culture Kid",
                 blurb: "Born in Korea, also raised in America, Germany, Malaysia",
                 current: false
             },
             {
                 preview: "Honest",
-                html: "<span style='color: purple;'>TEST</span>",
+                html: "<div class='wordBox sword'><div class='pentagon'><div class='recHor'></div><div class='recVer'></div></div><div class='preview'>Honesty</div></div>",
                 word: "Honest",
                 blurb: "What my friends say about me :)",
                 current: false
             },
             {
                 preview: "MBTI",
-                html: "<span style='color: green;'>TEST</span>",
+                html: "<div class='wordBox starwand'><div class='starshape'><div class='wand'></div></div><div class='preview'>MBTI</div></div>",
                 word: "ESTJ: The Executive",
                 blurb: "Insight of the Day: Executives are the least likely personality type to feel overwhelmed by their to-do list.",
                 current: false
