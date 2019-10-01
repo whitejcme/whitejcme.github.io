2e3e637cbfaef37e3942c4487384be1cd4964e48
Jiwon Juliet Yoon <jiwonjulietyoon@gmail.com>
2019-10-01 20:37:00 +0900

    Start on blog posts page

---
+++ src/components/bookPage/BlogPosts.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="bookPage">
+    <div class="pageContainer">
+      <h2 class="pageHeader">Blog Posts</h2>
+      <div class="pageContent">
+
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<script>
+import NavArrows from '@/components/NavArrows';
+
+export default {
+  name: 'BlogPosts',
+  components: {
+    NavArrows
+  },
+  data() {
+    return {
+      prev: "",
+      next: ""
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import "@/css/style.scss";
+
+</style>
